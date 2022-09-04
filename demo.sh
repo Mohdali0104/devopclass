db530c689feeddeb498dc165253011659d063306
mohdali0104 <mohadali0104@gmail.com>
2022-09-04 19:19:51 +0530

    update file

--- demo.sh
+++ demo.sh
@@ -13,15 +13,5 @@
   echo "********************8 Display jenkins memory space **********"
   free -m
 
-  echo "******************** display java memory space ***********"
-  free -m
-
-  echo "******************* display Tomcat8 memory space ****************"
-  free -m
-
-  echo "*************************** display apache2 memory space *******************"
-  free -m
-
-  echo "***************************** display maven memory space ******************"
-  free -m
+  
   
